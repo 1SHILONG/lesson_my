23b50c5d7d698c21c93f13196ade4d7420a638f6
1SHILONG <111036813+1SHILONG@users.noreply.github.com>
2022-11-13 16:39:21 +0800

    小程序开发

---
+++ weapp/discovery/pages/index/index.js
@@ -0,0 +1,54 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    title:'奔驰AMG',
+    car:'https://p3.dcarimg.com/img/tos-cn-i-0000c0030/7ce90daaf5934b5ea9bf1e554e910d6c~1200x0.png',
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUseGetUserProfile: false,
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    setTimeout(() => {
+      this.setData({
+        title: '奔驰C',
+        car:'https://p3.dcarimg.com/img/tos-cn-i-0000c0030/1ef1425685cd45d5b257dad5344e2523~1200x0.png'
+
+        
+      })
+    }, 3000)
+   
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
